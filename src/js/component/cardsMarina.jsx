76ee942e8c7f5505e2340938fcd908724ca69706
Marina Myssak <marina.myssak@outlook.com>
2024-01-11 06:27:52 +0600

    change text in Cards

--- src/js/component/cardsMarina.jsx
+++ src/js/component/cardsMarina.jsx
@@ -8,7 +8,7 @@ import React from "react";
                         <div className="card text-center">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to
+                            This is a wider card with supporting text above as a natural lead-in to
                             additional content. This content is a little bit longer.
                         </p>
                     </div>
